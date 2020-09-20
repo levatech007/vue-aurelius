3d86ecf6e43b88aae7592b13bca1537870ff5fb7
Eva-Liisa <eva@julianus.ee>
2020-09-20 13:13:47 +0300

    claim fetch from json server

--- src/store/modules/claims.js
+++ src/store/modules/claims.js
@@ -1,61 +1,30 @@
+import axios from 'axios'
+
 const state = {
-  todos: [
-    {
-      "todo": { "time":"11/14/1019 @ 11.33pm", "descr":"Maksekokkulepe muudetud", "user":"EVA" },
-      "comment": ""
-    },
-    {
-      "todo": { "time":"11/14/1019 @ 11.33pm", "descr":"Maksekokkulepe", "user":"EVA" },
-      "comment": ""
-    },
-    {
-      "todo": { "time":"11/14/1019 @ 11.33pm", "descr":"Email võlglasele", "user":"EVA" },
-      "comment":"Emaili teks siia"
-    },
-    {
-      "todo": { "time":"11/14/1019 @ 11.33pm", "descr":"Kliendi kommentaar", "user":"TEST" },
-      "comment":"Uus claim"
-    },
-    {
-      "todo": { "time":"11/14/1019 @ 11.33pm",  "descr":"Aktiivne", "user":"EVA" },
-      "comment":""
-    },
-    {
-      "todo": { "time":"11/14/1019 @ 11.33pm", "descr":"Aktiivne",  "user":"EVA" },
-      "comment":""
-    },
-    {
-      "todo": { "time":"11/14/1019 @ 11.33pm", "descr":"Lisati telefon 555-5555", "user":"TEST" },
-      "comment":""
-    },
-    {
-      "todo": { "time":"11/14/1019 @ 11.33pm", "descr":"Lisati email", "user":"EVA" },
-      "comment":""
-    },
-    {
-      "todo": { "time":"11/14/1019 @ 11.33pm", "descr":"RR Päring", "user":"EVA" },
-      "comment":"Aadress Main St.123"
-    }
-  ]
-}
-
-
+    claim: {}
+};
 
 const getters = {
-  allTodos: (state) => state.todos
-}
+    currentClaim: state => state.claim
+};
 
 const actions = {
-  // data call
-}
+    async fetchClaim({commit}){
+      const response = await axios.get("http://localhost:3000/claim");
+      commit("setClaim", response.data)
+      console.log(response)
+    }
+};
 
 const mutations = {
-
-}
+    setClaim: (state, claim) => (
+        state.claim = claim
+    )
+};
 
 export default {
-  state,
-  getters,
-  actions,
-  mutations
+    state,
+    getters,
+    actions,
+    mutations
 }
